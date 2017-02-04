60ab49f85a2ea4c870b4312fc0d559befa24450e
ameyanayak27 <ameyanayak27@gmail.com>
2017-02-04 01:42:12 -0500

    Add files via upload

--- CliTool.java
+++ CliTool.java
@@ -5,46 +5,58 @@ import java.util.Scanner;
 
 public class CliTool {
 	public static void main(String args[]) throws IOException {
-		//create otput files
+		// create otput files
 		ProcessData.process_data();
-		//Run CLI tool
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter User Name");
-		String usrname=sc.next();
-		System.out.println("Ente Choice\n1.Expense Repoprt\n2.Is a Student/Hobbies \n3.Compatibility");
-		int choice=sc.nextInt();
-		BufferedReader br=null;
-		String line="";
-		if(choice==1)
-		{
-			br=new BufferedReader(new FileReader("output_user/expense_"+usrname+".csv"));
-			//System.out.println("Month\tExpense\tEarnings");
-			while ((line = br.readLine()) != null) {
-				//Month,Expense,Earnings
-				String data[]=line.split(",");
-				System.out.println(data[0]+"\t"+data[1]+"\t"+data[2]);
-            }
+		// Run CLI tool
+		xyz: while (true) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter Choice\n1.Expense Repoprt\n2.Is a Student/Hobbies \n3.Compatibility \n4.Exit");
+			int choice = sc.nextInt();
+			BufferedReader br = null;
+			String line = "";
+			try {
+				switch (choice) {
+				case 1:
+					System.out.println("Enter User Name");
+					String usrname = sc.next();
+					br = new BufferedReader(new FileReader("output_expenses/expense_" + usrname + ".csv"));
+					// System.out.println("Month\tExpense\tEarnings");
+					while ((line = br.readLine()) != null) {
+						// Month,Expense,Earnings
+						String data[] = line.split(",");
+						System.out.println(data[0] + "\t" + data[1] + "\t" + data[2]);
+					}
+					break;
+				case 2:
+					System.out.println("Enter User Name");
+					usrname = sc.next();
+					br = new BufferedReader(new FileReader("output_hobbies/hobbies_" + usrname + ".csv"));
+					while ((line = br.readLine()) != null) {
+						// Month,Expense,Earnings
+						// String data[]=line.split(",");
+						System.out.println(line);
+					}
+					break;
+				case 3:
+					System.out.println("Enter User Name");
+					usrname = sc.next();
+					br = new BufferedReader(new FileReader("output_match/match_hobbies_" + usrname + ".csv"));
+					System.out.println("Compatibilities with:");
+					while ((line = br.readLine()) != null) {
+						// Month,Expense,Earnings
+						// String data[]=line.split(",");
+						System.out.println(line);
+					}
+					break;
+				case 4:
+					break xyz;
+				default:
+					System.out.println("invalid input");
+				}
+				br.close();
+			} catch (Exception e) {
+				System.out.println("invalid user");
+			}
 		}
-		if(choice==2)
-		{
-			br=new BufferedReader(new FileReader("output_user/hobbies_"+usrname+".csv"));
-			while ((line = br.readLine()) != null) {
-				//Month,Expense,Earnings
-				//String data[]=line.split(",");
-				System.out.println(line);
-            }
-		}
-		if(choice==3)
-		{
-			br=new BufferedReader(new FileReader("output_user/match_"+usrname+".csv"));
-			System.out.println("Compatibilities with:");
-			while ((line = br.readLine()) != null) {
-				//Month,Expense,Earnings
-				//String data[]=line.split(",");
-				System.out.println(line);
-            }
-		}
-		
 	}
-
 }
--- ProcessData.java
+++ ProcessData.java
@@ -3,35 +3,35 @@ import java.io.*;
 
 public class ProcessData {
 	public static void process_data() {
+		// input
 		File directory = new File("transaction-data");
 		File[] listOfFiles = directory.listFiles();
-		File dir_output = new File("output_user");
-		dir_output.mkdir();
-		File f = new File("output_user/expense.csv");
-		// f.createNewFile();
-		
-		HashMap<String, Double[]> mapExpenses = new HashMap<String, Double[]>();
+		// dir for expense report
+		File dir_output_expenses = new File("output_expenses");
+		dir_output_expenses.mkdir();
+		// dir for hobbies report
+		File dir_output_hobbies = new File("output_hobbies");
+		dir_output_hobbies.mkdir();
 
 		try {
-		for (File file : listOfFiles) {
-			HashMap<String, Hobbies> hobbies = new HashMap<String, Hobbies>();
+			for (File file : listOfFiles) {
+				HashMap<String, Hobbies> hobbies = new HashMap<String, Hobbies>();
+				// map for counting matches in hobbies
+				EnumMap<Hobbies, Integer> hobbiesCount = new EnumMap<Hobbies, Integer>(Hobbies.class);
+				for (Hobbies h : Hobbies.values()) {
+					for (String keyword : h.getKeywords().split(","))
+						hobbies.put(keyword, h);
+				}
+				for (Hobbies h : Hobbies.values()) {
+					hobbiesCount.put(h, 0);
+				}
+				boolean isStudent = false;
+				Scanner scanner;
 
-			EnumMap<Hobbies, Integer> hobbiesCount = new EnumMap<Hobbies, Integer>(Hobbies.class);
-			for (Hobbies h : Hobbies.values()) {
-				for (String keyword : h.getKeywords().split(","))
-					hobbies.put(keyword, h);
-			}
-			for (Hobbies h : Hobbies.values()) {
-				hobbiesCount.put(h, 0);
-			}
-			boolean isStudent = false;
-			Scanner scanner;
-			
 				// f.createNewFile();
 				if (file.isFile() && file.toString().endsWith(".csv")) {
-					System.out.println("abc");
-					File out_expenses = new File("output_user/expense_" + file.getName().toString());
-					File out_hobbies = new File("output_user/hobbies_" + file.getName().toString());
+					File out_expenses = new File("output_expenses/expense_" + file.getName().toString());
+					File out_hobbies = new File("output_hobbies/hobbies_" + file.getName().toString());
 					PrintWriter pw_expenses = new PrintWriter(out_expenses);
 					PrintWriter pw_hobbies = new PrintWriter(out_hobbies);
 
@@ -43,7 +43,7 @@ public class ProcessData {
 					sb_expenses.append("Month,Expenses,Earnings\n");
 					sb_hobbies.append("Hobbies\n");
 					String month_change = "";
-					
+
 					double monthly_expense = 0, total_expense = 0, earnings = 0, total_earnings = 0;
 					scanner = new Scanner(file);
 					scanner.useDelimiter(",|\\n");
@@ -63,104 +63,95 @@ public class ProcessData {
 						String location = scanner.next();
 						String d[] = date.split("/");
 						if (!month_change.equals(d[0] + "/" + d[2])) {
-							// System.out.println(month_change + " " +
-							// monthly_expense + " " + earnings);
+							// if the month changes update
 							sb_expenses.append(month_change + "," + monthly_expense + "," + earnings + "\n");
 							month_change = d[0] + "/" + d[2];
-							total_expense+=monthly_expense;
+							total_expense += monthly_expense;
 							total_earnings += earnings;
 							monthly_expense = 0;
 							earnings = 0;
 						}
 						if (!Character.isDigit(expense.charAt(0))) {
-							// System.out.println(expense);
 							monthly_expense += Double.parseDouble(expense.substring(1));
 						} else {
-							// System.out.println("ABC");
 							earnings += Double.parseDouble(expense);
 						}
 					}
-					String user = file.getName().substring(file.getName().lastIndexOf("_")+1,file.getName().lastIndexOf("."));
-					mapExpenses.put(user, new Double[]{total_expense,total_earnings}) ;
 					pw_expenses.print(sb_expenses);
 					pw_expenses.close();
-					// break;
-					for(Hobbies h:hobbiesCount.keySet() )
-						if(hobbiesCount.get(h)!=0) {
-							sb_hobbies.append(h.toString()+"\n");
+					for (Hobbies h : hobbiesCount.keySet())
+						if (hobbiesCount.get(h) != 0) {
+							sb_hobbies.append(h.toString() + "\n");
 						}
 					pw_hobbies.print(sb_hobbies);
 					pw_hobbies.close();
-				}
-		}
-		System.out.println(mapExpenses);
-		generateCompatibility(mapExpenses);
+					scanner.close();
 
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			// break;
-
-		}
-		
-
-	
-	public static void generateCompatibility(HashMap<String, Double[]> mapExpenses) throws IOException{
-		File directory = new File("output_user");
-		File[] listOfFiles = directory.listFiles();
-		HashMap<String, String> compatibility = new HashMap<String, String>();
-		
-		for (File file : listOfFiles) {
-			System.out.println(file);
-			if(file.getName().contains("hobbies")) {
-			File out_match = new File("output_user/match_" + file.getName().toString());
-			PrintWriter pw_match = new PrintWriter(out_match);
-
-			out_match.createNewFile();
-
-			Scanner scanner = new Scanner(file);
-			scanner.useDelimiter(",|\\n");
-			HashSet<String>hobbies = new HashSet<String>();
-			while (scanner.hasNext()) {
-					if(scanner.next().equals("Hobbies"))
-						continue;
-					else
-						hobbies.add(scanner.next());
-						
-					
-				
-			}
-			for (File other_file : listOfFiles) {
-				int common_hobbies = 0;
-
-				if(file!=other_file) {
-					Scanner scanner2 = new Scanner(other_file);
-					scanner2.useDelimiter(",");
-					String user = other_file.getName().substring(other_file.getName().lastIndexOf("_")+1,other_file.getName().lastIndexOf("."));
-					while (scanner2.hasNext()) {
-						String hobby = scanner2.next();
-						System.out.println(hobby);
-						if(hobbies.contains(hobby))
-							common_hobbies++;
-					}
-					if(common_hobbies>3) {
-						pw_match.println(user);						
-						
-					}
-					
 				}
 			}
-			pw_match.close();
+			generateCompatibility();
 
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// break;
 
-		}
-		}
-		
 	}
 
+	// generate comaptibility report
+	public static void generateCompatibility() throws IOException {
+		File directory = new File("output_hobbies");
+		File[] listOfFiles = directory.listFiles();
+		File dir_output_match = new File("output_match");
+		dir_output_match.mkdir();
+		HashMap<String, String> compatibility = new HashMap<String, String>();
+		for (File file : listOfFiles) {
+			if (file.getName().contains("hobbies")) {
+				File out_match = new File("output_match/match_" + file.getName().toString());
+				PrintWriter pw_match = new PrintWriter(out_match);
+				out_match.createNewFile();
+
+				Scanner scanner = new Scanner(file);
+				scanner.useDelimiter("\\n");
+				HashSet<String> hobbies = new HashSet<String>();
+				while (scanner.hasNext()) {
+					String x = scanner.next();
+					if (x.equals("Hobbies"))
+						continue;
+					else
+						hobbies.add(x);
+				}
+				StringBuffer sb = new StringBuffer();
+				sb.append("User,Compatibility\n");
+				for (File other_file : listOfFiles) {
+					int common_hobbies = 0;
+					if (file != other_file && other_file.getName().contains("hobbies")) {
+						Scanner scanner2 = new Scanner(other_file);
+						scanner2.useDelimiter("\\n");
+						String user = other_file.getName().substring(other_file.getName().lastIndexOf("_") + 1,
+								other_file.getName().lastIndexOf("."));
+						while (scanner2.hasNext()) {
+							String hobby = scanner2.next();
+							if (hobbies.contains(hobby)) {
+								common_hobbies++;
+								
+							}
+						}
+						// for every hobby match multiply with 0.1
+						sb.append(user + "," + (0.1 * common_hobbies) + "\n");
+						scanner2.close();
+					}
+				}
+				scanner.close();
+				pw_match.println(sb);
+				pw_match.close();
+			}
+		}
+
+	}
 
 }
